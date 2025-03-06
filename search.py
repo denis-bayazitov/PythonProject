357e16ca9bda49747edb020cc90a9e5e8d317afb
Denis <newdenis@gmail.com>
2025-03-06 09:17:03 +0100

    kakoe-to soobshenie

--- search.py
+++ search.py
@@ -65,6 +65,9 @@ def search_by_actor():
     log_search("By Actor", actor_name)
 
 
+import datetime
+
+
 def search_by_genre_and_year():
     conn, cursor = get_db_connection()
     cursor.execute("SELECT category_id, name FROM category")
@@ -75,11 +78,24 @@ def search_by_genre_and_year():
         return
 
     year = input("Enter release Year: ")
+
+    # Проверка, что год введен корректно
     if not year.isdigit():
         print("\nInvalid Year format. Please enter a numeric Year.")
         print("****************************")
         return
 
+    # Преобразуем год в целое число
+    year = int(year)
+
+    # Получаем текущий год
+    current_year = datetime.datetime.now().year
+
+    # Проверяем, что год в пределах допустимого диапазона
+    if year < 1800 or year > current_year:
+        print(f"\nInvalid Year. Please enter a year between 1895 and {current_year}.")
+        print("****************************")
+        return
 
     query = """
         SELECT f.title FROM film f
@@ -99,6 +115,42 @@ def search_by_genre_and_year():
     print("****************************")
 
     log_search("By Genre and Year", f"{category_name}, {year}")
+
+
+# def search_by_genre_and_year():
+#     conn, cursor = get_db_connection()
+#     cursor.execute("SELECT category_id, name FROM category")
+#     categories = cursor.fetchall()
+#
+#     category_id, category_name = get_valid_category(categories)
+#     if not category_id:
+#         return
+#
+#     year = input("Enter release Year: ")
+#     if not year.isdigit():
+#         print("\nInvalid Year format. Please enter a numeric Year.")
+#         print("****************************")
+#         return
+#
+#
+#     query = """
+#         SELECT f.title FROM film f
+#         JOIN film_category fc ON f.film_id = fc.film_id
+#         WHERE fc.category_id = %s AND f.release_year = %s LIMIT 5;
+#     """
+#     cursor.execute(query, (category_id, year))
+#     films = cursor.fetchall()
+#
+#     if handle_empty_result(films, "Genre and Year", f"{category_name}, {year}"):
+#         return
+#
+#     print(f"\nSearch results for Genre {category_name} and Year {year}:")
+#     for film in films:
+#         print(film[0])
+#
+#     print("****************************")
+#
+#     log_search("By Genre and Year", f"{category_name}, {year}")
 
 
 def search_by_keyword():
